cd34388ded80cd106920bbd3ac073cbd3de3c7c0
Ben Marshall <ben3005@hotmail.com>
2016-10-19 15:21:00 +0100

    Logic changed for logging error in windows

--- src/windows/OnefileErrorLogging.js
+++ src/windows/OnefileErrorLogging.js
@@ -1,13 +1,18 @@
 (function () {
-    "use strict";
-    var OnefileErrorLoggingProxy = {
+	"use strict";
+	var winNetConn = Windows.Networking.Connectivity;
+	var networkInfo = winNetConn.NetworkInformation;
+
+	var OnefileErrorLoggingProxy = {
 		logError: function (win, fail, args, env) {
 			try {
 				if (!args[0]) {
 					fail("Missing options");
 				}
 				var argConfig = args[0];
-				logError(argConfig)
+				networkInfo.addEventListener("networkstatuschanged", tryUploadErrors);
+				logErrorToServer(argConfig)
+					.then(success, saveErrorToDb)
 					.done(success, fail);
 			} catch (e) {
 				fail(e);
@@ -17,29 +22,59 @@
 				win(result);
 			}
 		}
-    };
+	};
 
-    function logError(config) {
-    	var options = {
-    		url: config.endpoint,
-    		type: 'POST',
-    		headers: {
-    			'X-UserID': config.headers.userId,
-    			'X-Current-Platform': config.headers.currentPlatform,
-    			'X-Current-Platform-Version': config.headers.currentPlatformVersion,
-    			"Content-type": "application/json"
-    		},
-    		data: JSON.stringify({
-    			'Name': config.error.name,
-    			'Message': config.error.message,
-    			'Cause': config.error.cause,
-    			'StackTrace': config.error.stackTrace,
-    			'CurrentUsername': config.currentUsername
-    		})
-    	}
-    	return WinJS.xhr(options);
-    }
+	function saveErrorToDb(config) {
+		// save error to database
+	}
 
+	function tryUploadErrors() {
+		var profile = networkInfo.getInternetConnectionProfile();
 
-    require("cordova/exec/proxy").add("OnefileErrorLogging", OnefileErrorLoggingProxy);
+		if (profile) {
+			var conLevel = profile.getNetworkConnectivityLevel();
+			var interfaceType = profile.networkAdapter.ianaInterfaceType;
+
+			if (conLevel === Windows.Networking.Connectivity.NetworkConnectivityLevel.internetAccess) {
+				// get next error from the database
+				// log the error with the server
+			}
+		}
+	}
+
+	function logErrorToServer(config) {
+		var profile = networkInfo.getInternetConnectionProfile();
+
+		if (profile) {
+			var conLevel = profile.getNetworkConnectivityLevel();
+			var interfaceType = profile.networkAdapter.ianaInterfaceType;
+
+			if (conLevel === Windows.Networking.Connectivity.NetworkConnectivityLevel.internetAccess) {
+				var options = {
+					url: config.endpoint,
+					type: 'POST',
+					headers: {
+						'X-UserID': config.headers.userId,
+						'X-Current-Platform': config.headers.currentPlatform,
+						'X-Current-Platform-Version': config.headers.currentPlatformVersion,
+						"Content-type": "application/json"
+					},
+					data: JSON.stringify({
+						'Name': config.error.name,
+						'Message': config.error.message,
+						'Cause': config.error.cause,
+						'StackTrace': config.error.stackTrace,
+						'CurrentUsername': config.currentUsername
+					})
+				}
+				return WinJS.xhr(options);
+			} else {
+				return WinJS.Promise.wrapError(config);
+			}
+		} else {
+			return WinJS.Promise.wrapError(config);
+		}
+	}
+
+	require("cordova/exec/proxy").add("OnefileErrorLogging", OnefileErrorLoggingProxy);
 })();
