aded9cf558b7af08afafa053c2d9efd0687a5fd9
Martin Howarth <mhowarth@onefile.co.uk>
2017-02-13 10:18:43 +0000

    Fixed error logging, endless loop, fixes #1690

--- src/ios/OnefileErrorLogging.m
+++ src/ios/OnefileErrorLogging.m
@@ -2,6 +2,8 @@
 #import "database.h"
 #import "sendrequest.h"
 
+#define MAX_NUMBER_OF_ERRORS 1000
+
 /************************************************************************************************************
  *      OnefileErrorLogging - Initialisation point of the plugin.
  ************************************************************************************************************/
@@ -11,7 +13,6 @@
 
 - (void)pluginInitialize
 {
-    NSLog(@"OnefileErrorLogging - pluginInitialize");
     self.inUse = NO;
 }
 
@@ -23,17 +24,15 @@
     NSString *callbackId = command.callbackId;
     NSDictionary *config = [command argumentAtIndex:0];
     NSDictionary *response;
-
+    int processMaxErrors = MAX_NUMBER_OF_ERRORS;
+    int status = 0;
     if ([config isKindOfClass:[NSNull class]]) {
         config = [NSDictionary dictionary];
     }
-
     do {
         if([config count] > 0) {
             int ID = [[config objectForKey:@"ID"] intValue];
-
             response = [SendRequest makeRequest: config];
-            int status;
             if([[response objectForKey:@"status"] isKindOfClass: [NSNumber class]])
                 status = [[response objectForKey:@"status"] intValue];
             else
@@ -45,22 +44,22 @@
                     [database deleteErrorFromDB:db usingID: ID];
                 } else {
                 }
-                config = [database getErrorFromDB:db];
+                processMaxErrors--;
+                if(processMaxErrors > 0)
+                    config = [database getErrorFromDB:db];
                 [database closeDatabase: db];
             }
             else
             {
                 sqlite3 *db = [database createOpenDatabase];
                 [database createTable:db];
-                if(ID > 0) {
-                } else {
+                if(ID == 0) {
                     [database insertErrorWithDB:db config:config];
                 }
-                config = [database getErrorFromDB:db];
                 [database closeDatabase: db];
             }
         }
-    } while(config && [config count] > 0);
+    } while(processMaxErrors > 0 && status > 0 && config && [config count] > 0);
 
     CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary: response];
     [self.commandDelegate sendPluginResult:result callbackId:callbackId];
